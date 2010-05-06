be83b269a1c5f79005c9580087d81ce1303ab40c
Mezgani Ali <handrix@gmail.com>
2010-05-06 21:26:41 +0000

    main color

--- main.c
+++ main.c
@@ -3,10 +3,11 @@
 static 
 void usage(FILE *out, char *program)
 {
-
-  fprintf(out, "\033[0;34mUsage:\t  \%s    [address]/[prefix]\n",program);
+  fprintf(out, "\033[0;31m\nntcalc (network calculator) takes an IPv4 address and a prefix and calculates\n\
+the resulting broadcast, network, Cisco wildcard mask, and host/net range.\n\n\033[0m");
+  fprintf(out, "\033[0;34mUsage:\t  \%s     address/prefix\n",program);
   fprintf(out, "Example:  %s     192.168.1.2/24\n\n\033[0m", program);
-  fprintf(out, "\033[0;31mntcalc (network calculator) takes an IPv4 address and a prefix and calculates \nthe resulting broadcast, network, Cisco wildcard mask, and host/net range.\n\033[0m");
+
 
   
 }
@@ -29,7 +30,7 @@ int main(int argc, char **argv)
 {
 
   char *address, *pre;
-  //data_t *ptr=NULL;
+  //data_t *ptr=NULL,**node;
   uint32_t a,b,c,d,e,f;
   unsigned long value;
   int prefix;
@@ -73,30 +74,33 @@ int main(int argc, char **argv)
 
   // I like output color :)
 
-  printf("\033[1;34m*\033[0m");
-  printf(" Address      : %s\n", address);
-  printf("\033[1;34m*\033[0m");
-  printf(" Netmask      : %s\n", int2string(a));
-  printf("\033[1;34m*\033[0m");
-  printf(" Broadcast    : %s\n", int2string(b));
-  printf("\033[1;34m*\033[0m");
-  printf(" Wildcard     : %s\n", int2string(d));
-  printf("\033[1;34m*\033[0m");
-  printf(" Network      : %s/%s\n", int2string(c),pre);
-  printf("\033[1;34m*\033[0m");
-  printf(" Hostmin      : %s\n", int2string(e));
-  printf("\033[1;34m*\033[0m");
-  printf(" Hostmax      : %s\n", int2string(f));
-  printf("\033[1;34m*\033[0m");
-  printf(" Hosts/nets   : %d\n", nethost(prefix));
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Address      :\033[0m \033[0;35m%s\033[0m\n", address);
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Netmask      :\033[0m \033[0;35m%s\033[0m\n", int2string(a));
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Broadcast    :\033[0m \033[0;35m%s\033[0m\n", int2string(b));
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Wildcard     :\033[0m \033[0;35m%s\033[0m\n", int2string(d));
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Network      :\033[0m \033[0;35m%s/%s\033[0m\n", int2string(c),pre);
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Hostmin      :\033[0m \033[0;35m%s\033[0m\n", int2string(e));
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Hostmax      :\033[0m \033[0;35m%s\033[0m\n", int2string(f));
+  printf("\033[0;34m[+]\033[0m");
+  printf("\033[0;32m Hosts/Nets   :\033[0m \033[0;35m%d\033[0m\n", nethost(prefix));
 
-  /*
-    ptr=gethosts(value, prefix);
 
-  if (ptr==NULL){
+
+/*
+  ptr=ggethosts(NULL,value, prefix);
+
+  if (ptr==NULL)
     die("Emty data\n");
-  }    
-  display(ptr);
+   
+  
+   display(ptr);
   */
 
   return 0;
